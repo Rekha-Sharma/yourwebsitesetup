ad7a7920382011825bbc4b70187e23482efba746
Mohit Chandel <45650327+mohitchandel@users.noreply.github.com>
2021-04-10 12:41:38 +0530

    Update Navigation.js

--- gui/src/Components/Navigation.js
+++ gui/src/Components/Navigation.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+
 
 const Navigation = () =>{
     return(
         <div>
-
+            <Button className="btn-danger">
+                The button
+            </Button>
         </div>
     )
 }
